162648579ca7f505e13189a6b0ed47d9432de28f
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-08-24 14:52:09 +0200

    Refactor error handling in index.js

--- script.js
+++ script.js
@@ -2,20 +2,32 @@ const form = document.querySelector("form");
 const inputEmail = document.querySelector(".form-input");
 const errorMessage = document.querySelector(".form-label-span");
 
+const cardContainer = document.querySelector(".card-container");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  formValidation();
-});
 
-function formValidation() {
   const userInput = inputEmail.value;
   const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   if (!userInput.match(mailFormat)) {
-    inputEmail.classList.add("form-input-error--mod");
-    errorMessage.classList.add("form-label-span-error--mod");
+    errorHandling();
   } else {
-    inputEmail.classList.remove("form-input-error--mod");
-    errorMessage.classList.remove("form-label-span-error--mod");
+    errorHandlingDelete();
+    removeCardContainer();
   }
+});
+
+function errorHandling() {
+  inputEmail.classList.add("form-input-error--mod");
+  errorMessage.classList.add("form-label-span-error--mod");
+}
+
+function errorHandlingDelete() {
+  inputEmail.classList.remove("form-input-error--mod");
+  errorMessage.classList.remove("form-label-span-error--mod");
+}
+
+function removeCardContainer() {
+  cardContainer.style.display = "none";
 }
